e2f75b9be093caaf5c2a0543a2ab48cfda0ae696
Biraj Diyora <birajdiyora1@gmail.com>
2024-10-09 20:35:31 +0530

    commit

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.contrib.auth import login as auth_login,authenticate
 from .EmailBackend import EmailBackend
 from .models import Attendance, Session, Subject
 
@@ -23,88 +23,88 @@ def login_page(request):
     return render(request, 'main_app/login.html')
 
 
-# def doLogin(request, **kwargs):
-#     if request.method != 'POST':
-#         return HttpResponse("<h4>Denied</h4>")
-#     else:
-#         #Google recaptcha
-#         captcha_token = request.POST.get('g-recaptcha-response')
-#         captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-#         captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
-#         data = {
-#             'secret': captcha_key,
-#             'response': captcha_token
-#         }
-#         # Make request
-#         try:
-#             captcha_server = requests.post(url=captcha_url, data=data)
-#             response = json.loads(captcha_server.text)
-#             if response['success'] == False:
-#                 messages.error(request, 'Invalid Captcha. Try Again')
-#                 return redirect('/')
-#         except:
-#             messages.error(request, 'Captcha could not be verified. Try Again')
-#             return redirect('/')
-        
-#         #Authenticate
-#         user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
-#         if user != None:
-#             login(request, user)
-#             if user.user_type == '1':
-#                 return redirect(reverse("admin_home"))
-#             elif user.user_type == '2':
-#                 return redirect(reverse("staff_home"))
-#             else:
-#                 return redirect(reverse("student_home"))
-#         else:
-#             messages.error(request, "Invalid details")
-#             return redirect("/")
-from django.shortcuts import redirect
-from django.http import HttpResponse
-from django.contrib import messages
-from django.contrib.auth import login
-from django.urls import reverse
-import requests
-import json
-
 def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
-    
-    # Google reCAPTCHA verification
-    captcha_token = request.POST.get('g-recaptcha-response')
-    captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-    captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
-    data = {
-        'secret': captcha_key,
-        'response': captcha_token
-    }
-
-    try:
-        captcha_server = requests.post(url=captcha_url, data=data)
-        response = json.loads(captcha_server.text)
-        if not response.get('success', False):
-            messages.error(request, 'Invalid Captcha. Try Again')
-            return redirect('/')
-    except Exception as e:
-        messages.error(request, 'Captcha could not be verified. Try Again')
-        return redirect('/')
-
-    # Authenticate user
-    user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
-    
-    if user is not None:
-        login(request, user)
-        # Redirect based on user type
-        if user.user_type == '1':  # Admin
-            return redirect(reverse("admin_home"))
-        elif user.user_type == '2':  # Staff
-            return redirect(reverse("staff_home"))
-        else:  # Student
-            return redirect(reverse("student_home"))
     else:
-        messages.error(request, "Invalid email or password")
-        return redirect("/")
+        #Google recaptcha
+        # captcha_token = request.POST.get('g-recaptcha-response')
+        # captcha_url = "https://www.google.com/recaptcha/api/siteverify"
+        # captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
+        # data = {
+        #     'secret': captcha_key,
+        #     'response': captcha_token
+        # }
+        # Make request
+        # try:
+        #     captcha_server = requests.post(url=captcha_url, data=data)
+        #     response = json.loads(captcha_server.text)
+        #     if response['success'] == False:
+        #         messages.error(request, 'Invalid Captcha. Try Again')
+        #         return redirect('/')
+        # except:
+        #     messages.error(request, 'Captcha could not be verified. Try Again')
+        #     return redirect('/')
+        
+        #Authenticate
+        user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user != None:
+            auth_login(request,user)
+            if user.user_type == '1':
+                return redirect(reverse("admin_home"))
+            elif user.user_type == '2':
+                return redirect(reverse("staff_home"))
+            else:
+                return redirect(reverse("student_home"))
+        else:
+            messages.error(request, "Invalid details")
+            return redirect("/")
+# from django.shortcuts import redirect
+# from django.http import HttpResponse
+# from django.contrib import messages
+# from django.contrib.auth import login
+# from django.urls import reverse
+# import requests
+# import json
+
+# def doLogin(request, **kwargs):
+#     if request.method != 'POST':
+#         return HttpResponse("<h4>Denied</h4>")
+    
+#     # Google reCAPTCHA verification
+#     captcha_token = request.POST.get('g-recaptcha-response')
+#     captcha_url = "https://www.google.com/recaptcha/api/siteverify"
+#     captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
+#     data = {
+#         'secret': captcha_key,
+#         'response': captcha_token
+#     }
+
+#     try:
+#         captcha_server = requests.post(url=captcha_url, data=data)
+#         response = json.loads(captcha_server.text)
+#         if not response.get('success', False):
+#             messages.error(request, 'Invalid Captcha. Try Again')
+#             return redirect('/')
+#     except Exception as e:
+#         messages.error(request, 'Captcha could not be verified. Try Again')
+#         return redirect('/')
+
+#     # Authenticate user
+#     user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+    
+#     if user is not None:
+#         login(request, user)
+#         # Redirect based on user type
+#         if user.user_type == '1':  # Admin
+#             return redirect(reverse("admin_home"))
+#         elif user.user_type == '2':  # Staff
+#             return redirect(reverse("staff_home"))
+#         else:  # Student
+#             return redirect(reverse("student_home"))
+#     else:
+#         messages.error(request, "Invalid email or password")
+#         return redirect("/")
 
 
 
@@ -173,12 +173,15 @@ messaging.setBackgroundMessageHandler(function (payload) {
     """
     return HttpResponse(data, content_type='application/javascript')
 
+from django.shortcuts import render
+from .models import *
 
 def index(request):
     return render(request, 'frontend/index.html')
 
 def courses(request):
-    return render(request, 'frontend/courses.html')
+    all_courses = Course.objects.all()  
+    return render(request, 'frontend/courses.html', {'courses': all_courses})
 
 def contact(request):
     return render(request, 'frontend/contact.html')
